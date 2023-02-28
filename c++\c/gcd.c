fa75fa0dc554dce23615d283ad54b42dd1432189
kushal <kushalsubedi55@gmail.com>
2023-02-28 13:19:48 +0545

    gcd change

--- c++\c/gcd.c
+++ c++\c/gcd.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 
 int gcd(int a ,int b ){
-  
+  int r;
   while(a!=b){
-    if (a>b){
-        a=a-b;
-    }
-
-    if (b>a){
-        b=b-a;
-    }
+  if (b==0) return a;
+  else {
+    r= a%b;
+   a=b;
+   b=r;
+  }
+   
 
   }
-return a;
+return r;
 
 }
 int main(){
